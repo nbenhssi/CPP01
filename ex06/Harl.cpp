4cbc0b0cfcd424d662495618fe3f916043dd2f46
nbenhssi <nourabenhssine10@gmail.com>
2025-12-27 13:54:28 +0100

    hh

---
+++ ex06/Harl.cpp
@@ -0,0 +1,61 @@
+#include <Harl.hpp>
+
+void Harl::debug()
+{ 
+    std::cout << "[DEBUG]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info()
+{
+    std::cout << "[INFO]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money."
+    << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning()
+{
+    std::cout << "[WARNING]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for"
+    << "years, whereas you started working here just last month." << std::endl;
+}
+
+void Harl::error()
+{
+    std::cout << "[ERROR]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now."  <<std::endl
+}
+
+void fct()
+{
+    &Harl::debug();
+    &Harl::info();
+    &Harl::warning();
+    &Harl::error();
+}
+
+void Harl::complain(std::string level)
+{
+    std::string levels[]= {"DEBUG", "INFO", "WARNING", "ERROR"};
+    int index = -1;
+    for (int i = 0; i < 4; i++)
+    {
+        if (levels[i] == level)
+        {
+            index = i;
+        }
+    }
+    switch (index)
+    {
+        case 0:
+            debug();
+        case 1:
+            info();
+        case 2:
+            warning();
+        case 3:
+            error();
+        default:
+            std::cout<<"[ Probably complaining about insignificant problems ]"<<std::endl;
+    }
+}
